0bd52b8839a2231ca5eacc5f8d125ae18b35fbf5
shivsurya-s <95732517+shivsurya-s@users.noreply.github.com>
2022-01-15 15:02:34 +0530

    Create mins_to_secs.py

---
+++ Python/mins_to_secs.py
@@ -0,0 +1,11 @@
+def mins_to_secs():
+    minutes = float(input())
+    seconds = minutes*60
+    print(seconds)
+mins_to_secs()
+
+#2nd methd:
+mins=int(input())
+for i in range (mins):
+    secs = mins*60
+print(secs)
